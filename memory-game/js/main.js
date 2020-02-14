4e7cc0b00f4e793fa4f92170b6805155647daa15
Juan Delgado <Delgado-juan@hotmail.com>
2020-02-14 16:47:05 -0500

    created game logic

--- memory-game/js/main.js
+++ memory-game/js/main.js
@@ -1,8 +1,27 @@
-console.log("Up and Running!");
+// console.log("Up and Running!");
 
-let cardOne = "queen";
-let cardTwo = "queen";
-let cardThree = "king";
-let cardFour = "king";
+// let cardOne = "queen";
+// let cardTwo = "queen";
+// let cardThree = "king";
+// let cardFour = "king";
 
-console.log("User flipped " + cardOne);
+// console.log("User flipped " + cardOne);
+
+let cards = ["queen", "queen", "king", "king"];
+let cardsInPlay = [];
+let cardOne = cards[0];
+cardsInPlay.push(cardOne);
+console.log(`User flipped ${cardsInPlay[0]}.`);
+let cardTwo = cards[2];
+cardsInPlay.push(cardTwo);
+console.log(`User flipped ${cardsInPlay[1]}.`);
+
+
+if(cardsInPlay.length ===  2){   
+    if(cardsInPlay[0] === cardsInPlay[1]){
+        alert('You found match!');
+    }else{
+        alert(`Sorry, try again`);
+    }
+
+}
